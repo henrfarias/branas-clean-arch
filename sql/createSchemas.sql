9004a53238743765593cee1b4735e23911d22fab
henrfarias <henrique.biasibetti@gmail.com>
2022-01-20 22:45:36 -0300

    chore: add sql script

---
+++ sql/createSchemas.sql
@@ -0,0 +1,46 @@
+ALTER SCHEMA public RENAME TO template;
+CREATE SCHEMA project;
+
+CREATE TABLE template.orders (
+  id serial,
+  coupon text,
+  code text,
+  cpf text,
+  issue_date timestamp,
+  freight numeric,
+  sequence integer,
+  primary key (id)
+);
+
+CREATE TABLE template.items (
+  id serial,
+  category text,
+  description,
+  price numeric,
+  width integer,
+  height integer,
+  length integer,
+  weight integer,
+  primary key (id)
+);
+
+INSERT INTO template.items (category, description, price, width, height, length, weight) values ('Instrumentos Musicais', 'Guitarra', 1000, 100, 50, 15, 3);
+INSERT INTO template.items (category, description, price, width, height, length, weight) values ('Instrumentos Musicais', 'Amplificador', 5000, 50, 50, 50, 22);
+INSERT INTO template.items (category, description, price, width, height, length, weight) values ('Acessórios', 'Cabo', 30, 10, 10, 10, 1);
+
+CREATE TABLE template.coupons (
+  code text,
+	percentage numeric,
+	expire_date timestamp,
+	primary key (code)
+);
+
+INSERT INTO template.coupons (code, percentage, expire_date) values ('VALE20', 20, '2021-10-10T10:00:00');
+
+CREATE TABLE template.order_items (
+  id_order integer,
+	id_item integer,
+	price numeric,
+	quantity integer,
+	primary key (id_order, id_item)
+)
