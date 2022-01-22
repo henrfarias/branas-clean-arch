780ecfd9f68737dda7bf0bb595c8b80221455bab
henrfarias <henrique.biasibetti@gmail.com>
2022-01-21 23:06:14 -0300

    chore: add commands to sql script

--- sql/createSchemas.sql
+++ sql/createSchemas.sql
@@ -1,5 +1,4 @@
 ALTER SCHEMA public RENAME TO template;
-CREATE SCHEMA project;
 
 CREATE TABLE template.orders (
   id serial,
@@ -38,6 +37,52 @@ CREATE TABLE template.coupons (
 INSERT INTO template.coupons (code, percentage, expire_date) values ('VALE20', 20, '2021-10-10T10:00:00');
 
 CREATE TABLE template.order_items (
+  id_order integer,
+	id_item integer,
+	price numeric,
+	quantity integer,
+	primary key (id_order, id_item)
+)
+
+CREATE SCHEMA project;
+
+CREATE TABLE project.orders (
+  id serial,
+  coupon text,
+  code text,
+  cpf text,
+  issue_date timestamp,
+  freight numeric,
+  sequence integer,
+  primary key (id)
+);
+
+CREATE TABLE project.products (
+  id serial,
+  category text,
+  description,
+  price numeric,
+  width integer,
+  height integer,
+  length integer,
+  weight integer,
+  primary key (id)
+);
+
+INSERT INTO project.items (category, description, price, width, height, length, weight) values ('Instrumentos Musicais', 'Guitarra', 1000, 100, 50, 15, 3);
+INSERT INTO project.items (category, description, price, width, height, length, weight) values ('Instrumentos Musicais', 'Amplificador', 5000, 50, 50, 50, 22);
+INSERT INTO project.items (category, description, price, width, height, length, weight) values ('Acessórios', 'Cabo', 30, 10, 10, 10, 1);
+
+CREATE TABLE project.coupons (
+  code text,
+	percentage numeric,
+	expire_date timestamp,
+	primary key (code)
+);
+
+INSERT INTO project.coupons (code, percentage, expire_date) values ('VALE20', 20, '2021-10-10T10:00:00');
+
+CREATE TABLE project.order_items (
   id_order integer,
 	id_item integer,
 	price numeric,
