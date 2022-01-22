d4a8235f5a3f9f335e121832e2cacace644bff0e
henrfarias <henrique.biasibetti@gmail.com>
2022-01-21 23:41:33 -0300

    chore: add commands to sql script

--- sql/createSchemas.sql
+++ sql/createSchemas.sql
@@ -14,7 +14,7 @@ CREATE TABLE template.orders (
 CREATE TABLE template.items (
   id serial,
   category text,
-  description,
+  description text,
   price numeric,
   width integer,
   height integer,
@@ -42,7 +42,7 @@ CREATE TABLE template.order_items (
 	price numeric,
 	quantity integer,
 	primary key (id_order, id_item)
-)
+);
 
 CREATE SCHEMA project;
 
@@ -59,9 +59,9 @@ CREATE TABLE project.orders (
 
 CREATE TABLE project.products (
   id serial,
-  category text,
-  description,
+  name text,
   price numeric,
+  description text,
   width integer,
   height integer,
   length integer,
@@ -69,9 +69,9 @@ CREATE TABLE project.products (
   primary key (id)
 );
 
-INSERT INTO project.items (category, description, price, width, height, length, weight) values ('Instrumentos Musicais', 'Guitarra', 1000, 100, 50, 15, 3);
-INSERT INTO project.items (category, description, price, width, height, length, weight) values ('Instrumentos Musicais', 'Amplificador', 5000, 50, 50, 50, 22);
-INSERT INTO project.items (category, description, price, width, height, length, weight) values ('Acessórios', 'Cabo', 30, 10, 10, 10, 1);
+INSERT INTO project.products (name, price, description, width, height, length, weight) values ('lápis', 2, 'Faber-castell', 10, 15, 20, 1);
+INSERT INTO project.products (name, price, description, width, height, length, weight) values ('caneta', 3.7, 'Pilot', 10, 30, 100, 3);
+INSERT INTO project.products (name, price, description, width, height, length, weight) values ('caderno', 20, 'Tilibra', 50, 100, 200, 40);
 
 CREATE TABLE project.coupons (
   code text,
@@ -88,4 +88,4 @@ CREATE TABLE project.order_items (
 	price numeric,
 	quantity integer,
 	primary key (id_order, id_item)
-)
+);
